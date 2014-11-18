b824ac8a80c4b715a86f4a9d0718e94c547a44f8
Jan Karger <punkerat76@gmail.com>
2014-11-18 19:47:01 +0100

    add ToolTipAutoMoveSample

---
+++ ToolTipAutoMoveSample/ToolTipAutoMoveSample/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolTipAutoMoveSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseMove(object sender, MouseEventArgs e)
+        {
+            tt.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
+            tt.HorizontalOffset = e.GetPosition((IInputElement)sender).X + 16;
+            tt.VerticalOffset = e.GetPosition((IInputElement)sender).Y + 16;
+        }
+    }
+}
