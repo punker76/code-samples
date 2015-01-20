a35dcabc496ad45ce972e50a5fe79356d323f196
Jan Karger <punkerat76@gmail.com>
2015-01-20 10:13:24 +0100

    add ToggleFullScreen sample

--- MahAppsMetroSample/MahAppsMetroSample/App.xaml.cs
+++ MahAppsMetroSample/MahAppsMetroSample/App.xaml.cs
@@ -8,10 +8,10 @@ using System.Windows;
 
 namespace MahAppsMetroSample
 {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
-  public partial class App : Application
-  {
-  }
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
 }
--- MahAppsMetroSample/MahAppsMetroSample/MainWindow.xaml.cs
+++ MahAppsMetroSample/MahAppsMetroSample/MainWindow.xaml.cs
@@ -1,13 +1,56 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using MahApps.Metro.Controls;
 
 namespace MahAppsMetroSample
 {
-  public partial class MainWindow : MetroWindow
-  {
-    public MainWindow()
+    public partial class MainWindow : MetroWindow
     {
-      InitializeComponent();
+        public MainWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ToggleFullScreenProperty =
+            DependencyProperty.Register("ToggleFullScreen",
+                                        typeof(bool),
+                                        typeof(MainWindow),
+                                        new PropertyMetadata(default(bool), ToggleFullScreenPropertyChangedCallback));
+
+        private static void ToggleFullScreenPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var metroWindow = (MetroWindow)dependencyObject;
+            if (e.OldValue != e.NewValue)
+            {
+                var fullScreen = (bool)e.NewValue;
+                if (fullScreen)
+                {
+                    metroWindow.UseNoneWindowStyle = true;
+                    metroWindow.IgnoreTaskbarOnMaximize = true;
+                    metroWindow.ShowMinButton = false;
+                    metroWindow.ShowMaxRestoreButton = false;
+                    metroWindow.ShowCloseButton = false;
+                    metroWindow.WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    metroWindow.UseNoneWindowStyle = false;
+                    metroWindow.ShowTitleBar = true; // <-- this must be set to true
+                    metroWindow.IgnoreTaskbarOnMaximize = false;
+                    metroWindow.ShowMinButton = true;
+                    metroWindow.ShowMaxRestoreButton = true;
+                    metroWindow.ShowCloseButton = true;
+                    metroWindow.WindowState = WindowState.Normal;
+                }
+            }
+        }
+
+        public bool ToggleFullScreen
+        {
+            get { return (bool)GetValue(ToggleFullScreenProperty); }
+            set { SetValue(ToggleFullScreenProperty, value); }
+        }
     }
-  }
 }
