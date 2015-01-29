8fba46628234335f8b1e3221cd905a9d6583acd6
Jan Karger <punkerat76@gmail.com>
2015-01-29 22:18:56 +0100

    add MahAppsMetroStyleOnlyWindowSample

---
+++ MahAppsMetroStyleOnlyWindowSample/MahAppsMetroSample/CleanWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MahAppsMetroSample
+{
+    /// <summary>
+    /// Interaction logic for CleanWindow.xaml
+    /// </summary>
+    public partial class CleanWindow : MetroWindow
+    {
+        public CleanWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MahAppsMetroStyleOnlyWindowSample/MahAppsMetroSample/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace MahAppsMetroSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            new CleanWindow() { Owner = this }.ShowDialog();
+        }
+    }
+}
