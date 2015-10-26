12a5d2606b45fb5eaadf06acf75ccfae35f13d33
Jan Karger <punkerat76@gmail.com>
2015-10-26 14:46:48 +0100

    MahApps paket dependency

--- Build.cmd
+++ Build.cmd
@@ -7,8 +7,8 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 
 @echo on
 
-.paket/paket.bootstrapper.exe
-.paket/paket.exe update
+.paket\paket.bootstrapper.exe
+.paket\paket.exe update
 
 msbuild.exe /ToolsVersion:4.0 "MahAppsMetroDataGridSample\MahAppsMetroDataGridSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "MahAppsMetroSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
