3f528f9968a821407257e02d6b9a200fcb24e547
Jan Karger <punkerat76@gmail.com>
2015-10-26 13:57:39 +0100

    add paket stuff

--- Build.cmd
+++ Build.cmd
@@ -1,5 +1,14 @@
+@echo off
+
+IF NOT "%VS110COMNTOOLS%" == "" (call "%VS110COMNTOOLS%vsvars32.bat")
+IF NOT "%VS120COMNTOOLS%" == "" (call "%VS120COMNTOOLS%vsvars32.bat")
+IF NOT "%VS130COMNTOOLS%" == "" (call "%VS130COMNTOOLS%vsvars32.bat")
+IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
+
 @echo on
-call "%VS120COMNTOOLS%vsvars32.bat"
+
+.paket/paket.bootstrapper.exe
+.paket/paket.exe update
 
 msbuild.exe /ToolsVersion:4.0 "MahAppsMetroDataGridSample\MahAppsMetroDataGridSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "MahAppsMetroSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
