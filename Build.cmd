7a6f34eea3946af0611ef0f1ec21b21b074be223
Jan Karger <punkerat76@gmail.com>
2016-03-11 13:08:03 +0100

    MahAppsMetroFullScreen

--- Build.cmd
+++ Build.cmd
@@ -1,8 +1,5 @@
 @echo off
 
-IF NOT "%VS110COMNTOOLS%" == "" (call "%VS110COMNTOOLS%vsvars32.bat")
-IF NOT "%VS120COMNTOOLS%" == "" (call "%VS120COMNTOOLS%vsvars32.bat")
-IF NOT "%VS130COMNTOOLS%" == "" (call "%VS130COMNTOOLS%vsvars32.bat")
 IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 
 @echo on
@@ -10,9 +7,10 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 .paket\paket.bootstrapper.exe
 .paket\paket.exe update
 
-msbuild.exe /ToolsVersion:4.0 "MahAppsMetroDataGridSample\MahAppsMetroDataGridSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:4.0 "MahAppsMetroSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:4.0 "MahAppsMetroStyleOnlyWindowSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:4.0 "MahAppsMetroThemesSample\MahAppsMetroThemesSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:4.0 "MahAppsMetroVBSample\MahAppsMetroVBSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
-msbuild.exe /ToolsVersion:4.0 "ToolTipAutoMoveSample\ToolTipAutoMoveSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroDataGridSample\MahAppsMetroDataGridSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroFullScreen\MahAppsMetroFullScreen.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroStyleOnlyWindowSample\MahAppsMetroSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroThemesSample\MahAppsMetroThemesSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "MahAppsMetroVBSample\MahAppsMetroVBSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
+msbuild.exe /tv:14.0 "ToolTipAutoMoveSample\ToolTipAutoMoveSample.sln" /p:configuration=Debug /p:platform="Any CPU" /m /t:Clean,Rebuild
---
+++ MahAppsMetroFullScreen/MahAppsMetroFullScreen/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace MahAppsMetroFullScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FullClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+            this.UseNoneWindowStyle = true;
+            this.IgnoreTaskbarOnMaximize = true;
+        }
+
+        private void NormalClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+            this.UseNoneWindowStyle = false;
+            this.ShowTitleBar = true; // <-- this must be set to true
+            this.IgnoreTaskbarOnMaximize = false;
+        }
+    }
+}
