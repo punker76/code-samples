dc5dffc0f99463bcb634d357c3f4e09ac7df5452
Jan Karger <jan.karger@inform-software.com>
2017-01-31 23:18:27 +0100

    HamburgerMenu reset sample

--- MahAppsMetroHamburgerMenu/HamburgerMenuApp.V3/MainWindow.xaml.cs
+++ MahAppsMetroHamburgerMenu/HamburgerMenuApp.V3/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using MahApps.Metro.Controls;
 
 namespace HamburgerMenuApp.V3
@@ -24,6 +11,12 @@ namespace HamburgerMenuApp.V3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            HamburgerMenuControl.SelectedIndex = -1;
+            HamburgerMenuControl.SelectedOptionsIndex = -1;
         }
     }
 }
