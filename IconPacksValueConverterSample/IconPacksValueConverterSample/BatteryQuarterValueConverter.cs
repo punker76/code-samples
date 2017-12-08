0ef2b799ebf8adb75e08903a19aaa51fb8260009
Jan Karger <punkerat76@gmail.com>
2017-12-08 11:17:23 +0100

    Add IconPacksValueConverterSample

---
+++ IconPacksValueConverterSample/IconPacksValueConverterSample/BatteryQuarterValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using MahApps.Metro.IconPacks;
+
+namespace IconPacksValueConverterSample
+{
+    public class BatteryQuarterValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // the value is in this case a double (comes from the Slider)
+            var percentage = (value as double?).GetValueOrDefault(0);
+            if (percentage < 25)
+            {
+                return PackIconFontAwesomeKind.BatteryEmpty;
+            }
+            else if (percentage < 50)
+            {
+                return PackIconFontAwesomeKind.BatteryQuarter;
+            }
+            else if (percentage < 75)
+            {
+                return PackIconFontAwesomeKind.BatteryHalf;
+            }
+            else if (percentage < 100)
+            {
+                return PackIconFontAwesomeKind.BatteryThreeQuarters;
+            }
+            return PackIconFontAwesomeKind.BatteryFull;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // nothing here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IconPacksValueConverterSample/IconPacksValueConverterSample/BatteryValueConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using MahApps.Metro.IconPacks;
+
+namespace IconPacksValueConverterSample
+{
+    public class BatteryValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // the value is in this case a double (comes from the Slider)
+            var percentage = (value as double?).GetValueOrDefault(0);
+            if (percentage < 10)
+            {
+                return PackIconMaterialKind.BatteryOutline;
+            }
+            else if (percentage < 20)
+            {
+                return PackIconMaterialKind.Battery10;
+            }
+            else if (percentage < 30)
+            {
+                return PackIconMaterialKind.Battery20;
+            }
+            else if (percentage < 40)
+            {
+                return PackIconMaterialKind.Battery30;
+            }
+            else if (percentage < 50)
+            {
+                return PackIconMaterialKind.Battery40;
+            }
+            else if (percentage < 60)
+            {
+                return PackIconMaterialKind.Battery50;
+            }
+            else if (percentage < 70)
+            {
+                return PackIconMaterialKind.Battery60;
+            }
+            else if (percentage < 80)
+            {
+                return PackIconMaterialKind.Battery70;
+            }
+            else if (percentage < 90)
+            {
+                return PackIconMaterialKind.Battery80;
+            }
+            else if (percentage < 100)
+            {
+                return PackIconMaterialKind.Battery90;
+            }
+            return PackIconMaterialKind.Battery;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // nothing here
+            throw new NotImplementedException();
+        }
+    }
+}
