8c12f5539f0336400f22e3b60cf8f4aa012afd57
punker76 <punkerat76@gmail.com>
2019-09-30 16:23:42 +0200

    Update to latest MahApps v2.0 alpha (except MahAppsMetroThemesSample)

--- MahAppsMetroHamburgerMenuNavigation/MahAppsMetroHamburgerMenuNavigation/Mvvm/BindableBase.cs
+++ MahAppsMetroHamburgerMenuNavigation/MahAppsMetroHamburgerMenuNavigation/Mvvm/BindableBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using MahApps.Metro.Controls;
 
 namespace MahAppsMetroHamburgerMenuNavigation.Mvvm
 {
@@ -12,6 +13,17 @@ namespace MahAppsMetroHamburgerMenuNavigation.Mvvm
         /// Multicast event for property change notifications.
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         /// <summary>
         /// Checks if a property already matches a desired value.  Sets the property and
@@ -32,17 +44,6 @@ namespace MahAppsMetroHamburgerMenuNavigation.Mvvm
             storage = value;
             this.OnPropertyChanged(propertyName);
             return true;
-        }
-
-        /// <summary>
-        /// Notifies listeners that a property value has changed.
-        /// </summary>
-        /// <param name="propertyName">Name of the property used to notify listeners.  This
-        /// value is optional and can be provided automatically when invoked from compilers
-        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- MahAppsMetroHamburgerMenuNavigation/MahAppsMetroHamburgerMenuNavigation/ViewModels/MenuItem.cs
+++ MahAppsMetroHamburgerMenuNavigation/MahAppsMetroHamburgerMenuNavigation/ViewModels/MenuItem.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using JetBrains.Annotations;
+using MahApps.Metro.Controls;
 using MahAppsMetroHamburgerMenuNavigation.Mvvm;
 
 namespace MahAppsMetroHamburgerMenuNavigation.ViewModels
 {
-    internal class MenuItem : BindableBase
+    internal class MenuItem : HamburgerMenuItem, INotifyPropertyChanged
     {
         private object _icon;
         private string _text;
@@ -43,5 +47,34 @@ namespace MahAppsMetroHamburgerMenuNavigation.ViewModels
         }
 
         public bool IsNavigation => this._navigationDestination != null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Checks if a property already matches a desired value.  Sets the property and
+        /// notifies listeners only when necessary.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="storage">Reference to a property with both getter and setter.</param>
+        /// <param name="value">Desired value for the property.</param>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers that
+        /// support CallerMemberName.</param>
+        /// <returns>True if the value was changed, false if the existing value matched the
+        /// desired value.</returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value)) return false;
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
