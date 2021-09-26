82bc008ac1c081dd7da5a2acd6104e9414483ffd
punker76 <punkerat76@gmail.com>
2021-09-26 21:46:51 +0200

    Change ListViewSwitchItemsPanel

---
+++ ListViewSwitchItemsPanel/EnumToBoolConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ListViewSample
+{
+    [ValueConversion(typeof(ListViewMode), typeof(bool))]
+    public sealed class EnumToBoolConverter : IValueConverter
+    {
+        /// <summary> Gets the default instance </summary>
+        public static readonly EnumToBoolConverter Default = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum enumVal && parameter is Enum paramVal)
+            {
+                var equal = System.Convert.ToInt64(paramVal) == 0
+                    ? System.Convert.ToInt64(enumVal) == 0
+                    : enumVal.HasFlag(paramVal);
+                return equal;
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(true, value)
+                ? parameter
+                : ((parameter != null) ? DefaultEnumValue(parameter.GetType()) : Binding.DoNothing);
+        }
+
+        public static object DefaultEnumValue(Type enumType)
+        {
+            if (enumType != null)
+            {
+                if (enumType.IsEnum)
+                {
+                    return Enum.GetValues(enumType).GetValue(0);
+                }
+
+                throw new ArgumentException("given type is not an enum");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ListViewSwitchItemsPanel/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ListViewSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
