f065cbb32a37360f203fd41e2bc0b982ed04fe6c
punker76 <punkerat76@gmail.com>
2021-09-27 23:11:14 +0200

    Change MahAppsMetroFullScreen

---
+++ MahAppsMetroFullScreen/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MahAppsMetroFullScreen
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
