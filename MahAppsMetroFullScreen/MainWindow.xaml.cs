3ecadbb177ab0400dfa77f8c22dba79d6b65050b
punker76 <punkerat76@gmail.com>
2024-04-18 08:34:31 +0200

    refactor: update MahAppsMetroFullScreen

--- MahAppsMetroFullScreen/MainWindow.xaml.cs
+++ MahAppsMetroFullScreen/MainWindow.xaml.cs
@@ -16,19 +16,22 @@ namespace MahAppsMetroFullScreen
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            this.SetCurrentValue(WindowStateProperty, WindowState.Maximized);
-            this.SetCurrentValue(IgnoreTaskbarOnMaximizeProperty, true);
+            this.FullClick(null, null);
         }
 
         private void FullClick(object sender, RoutedEventArgs e)
         {
             this.SetCurrentValue(WindowStateProperty, WindowState.Maximized);
+            this.SetCurrentValue(ShowTitleBarProperty, false); // <-- this must be set to false
+            this.SetCurrentValue(WindowButtonCommandsOverlayBehaviorProperty, OverlayBehavior.Never); // We don't want the overlay
             this.SetCurrentValue(IgnoreTaskbarOnMaximizeProperty, true);
         }
 
         private void NormalClick(object sender, RoutedEventArgs e)
         {
             this.SetCurrentValue(WindowStateProperty, WindowState.Normal);
+            this.SetCurrentValue(ShowTitleBarProperty, true); // <-- this must be set to true
+            this.SetCurrentValue(WindowButtonCommandsOverlayBehaviorProperty, OverlayBehavior.Always); // Set to Always to show the overlay
             this.SetCurrentValue(IgnoreTaskbarOnMaximizeProperty, false);
         }
     }
